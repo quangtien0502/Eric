dc3d6c152ff7352ebc9515277d7332d106d449b9
Tien <quangtien05022001@gmail.com>
2023-04-01 21:15:27 +0700

    1/4/2023

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,26 @@
-let handleLogin = (req, res) => {
+import userService from "../services/userService"
+
+let handleLogin = async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    if (!email || !password) {
+
+        return res.status(500).json({
+            errCode: 1,
+            message: " Missing one of these parameters"
+        })
+    }
+    let userData = await userService.handleUserLogin(email, password);
     return res.status(200).json({
-        message: 'hello world'
+
+
+        userData
+
     })
+
+
 }
+
 module.exports = {
     handleLogin: handleLogin
 }
